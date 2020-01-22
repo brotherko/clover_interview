eee2e75d8abdfb40269ca84d8f1914224ceb9887
brotherko <ko19950314@gmail.com>
2020-01-22 19:02:47 +0800

    refactor file helper

--- helpers.py
+++ helpers.py
@@ -1,25 +1,26 @@
 import sys
 import csv
+import os
 
 
 class FileHelper:
+
     @classmethod
-    def readFormatFile(self, path_to_file):
+    def readFile(self, path_to_file, func):
         try:
             with open(path_to_file) as fp:
-                rows = csv.reader(fp)
-                return list(rows)
-        except OSError:
-            print "Couldn't read this file"
+                return func(fp)
+        except FileNotFoundError as err:
+            print(err)
+        except OSError as err:
+            print(err)
         except:
             print "Unexpected error:", sys.exc_info()[0]
 
     @classmethod
+    def readFormatFile(self, path_to_file):
+        return self.readFile(path_to_file, lambda fp: list(csv.reader(fp)))
+
+    @classmethod
     def readDataFile(self, path_to_file):
-        try:
-            with open(path_to_file) as fp:
-                return [line.rstrip('\n') for line in fp]
-        except OSError:
-            print "Couldn't read this file"
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
+        return self.readFile(path_to_file, lambda fp: list(line.rstrip('\n') for line in fp))
