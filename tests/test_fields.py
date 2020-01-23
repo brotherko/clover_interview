d44b1b34b1e1f766ce8c3e25fa7d61b1918d4dad
brotherko <ko19950314@gmail.com>
2020-01-23 23:16:45 +0800

    add IntegerField test

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -20,3 +20,33 @@ class TestFields_valid(unittest.TestCase):
         field = IntegerField("test", 5, 4)
         expect = 12345
         self.assertEqual(field.parse(self.data), expect)
+
+
+class TestIntegerField(unittest.TestCase):
+    def setUp(self):
+        self.field = IntegerField("test", 5, 0)
+
+    def test_positive_integer(self):
+        data = "11111"
+        expect = 11111
+        self.assertEqual(self.field.parse(data), expect)
+
+    def test_positive_integer_with_plus(self):
+        data = "+1111"
+        expect = 1111
+        self.assertEqual(self.field.parse(data), expect)
+
+    def test_negative_integer(self):
+        data = "-1323"
+        expect = -1323
+        self.assertEqual(self.field.parse(data), expect)
+
+    def test_alpha(self):
+        data = "abcde"
+        expect = SystemExit
+        self.assertRaises(expect, self.field.parse, data)
+
+    def test_decimal(self):
+        data = "0.333"
+        expect = SystemExit
+        self.assertRaises(expect, self.field.parse, data)
