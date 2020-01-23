e50343c4e362737df89e7f15fb72bccdafc20dd6
brotherko <ko19950314@gmail.com>
2020-01-24 02:26:24 +0800

    refactor parser

--- lib/Parser.py
+++ lib/Parser.py
@@ -7,16 +7,17 @@ from lib.enums import DataType
 
 class Parser:
     def __init__(self, path_to_file):
-        self.data = []
-        self.fields = []
         self.loadFormat(path_to_file)
         self.debug()
 
     def loadFormat(self, path_to_file):
-        contents = FileHelper.readFormatFile(path_to_file)
-        start_at = 0
+        format_contents = FileHelper.readFormatFile(path_to_file)
+        self.fields = self._getFieldsFormat(format_contents)
 
-        for field_data_idx, field_data in enumerate(contents):
+    def _getFieldsFormat(self, format_contents):
+        fields = []
+        start_at = 0
+        for field_data_idx, field_data in enumerate(format_contents):
             try:
                 name, width, data_type = field_data
                 field = Field.create(name, width, start_at, data_type)
@@ -27,16 +28,22 @@ class Parser:
                 sys.exit("Unexpected Error in format defination on line {}: {}".format(
                     field_data_idx+2, e))
 
+            fields.append(field)
             start_at += field.width
-            self.fields.append(field)
+        return fields
 
     def loadData(self, path_to_file):
-        contents = FileHelper.readDataFile(path_to_file)
-        for row in contents:
+        data_contents = FileHelper.readDataFile(path_to_file)
+        self.data = self._getData(data_contents)
+
+    def _getData(self, data_contents):
+        data = []
+        for row in data_contents:
             rowMap = {}
             for field_idx, field in enumerate(self.fields):
                 rowMap[field.name] = field.parse(row)
-            self.data.append(rowMap)
+            data.append(rowMap)
+        return data
 
     def asObject(self):
         return self.data
